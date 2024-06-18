6a50141f61b4f267ddd23f7026d2c0126b4bb6f0
SatakshiWadhwa <satakshi wadhwa@gmail.com>
2024-06-18 17:17:31 +0530

    25ques

---
+++ ques25.py
@@ -0,0 +1,25 @@
+with open("C:/Users/Satakshi Wadhwa/OneDrive/Desktop/text2.txt",'r') as firstfile, open("C:/Users/Satakshi Wadhwa/OneDrive/Desktop/text file.txt",'a') as secondfile: 
+
+      for line in firstfile:
+
+             secondfile.write(line)
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
